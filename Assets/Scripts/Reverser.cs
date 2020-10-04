ad34e9d39f76677a053fdb442bca62f843c93a0c
Никита Цинклер <nickzinkler@gmail.com>
2020-10-04 17:10:24 +0300

    final level 1

---
+++ Assets/Scripts/Reverser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reverser : MonoBehaviour
+{
+    void OnPreCull () {
+        Camera.main.ResetWorldToCameraMatrix ();
+        Camera.main.ResetProjectionMatrix ();
+        Camera.main.projectionMatrix = Camera.main.projectionMatrix * Matrix4x4.Scale(new Vector3 (-1, 1, 1));
+    }
+  
+    void OnPreRender () {
+        GL.SetRevertBackfacing (true);
+    }
+  
+    void OnPostRender () {
+        GL.SetRevertBackfacing (false);
+    }
+}
